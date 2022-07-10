8959f843f465715d679730bbe2981bb3ba6fdc0a
Gabriel Teixeira <Gteixeiraca@gmail.com>
2022-07-10 20:31:40 -0300

    Final code with comments

--- tp03.cpp
+++ tp03.cpp
@@ -5,14 +5,14 @@ using namespace std;
 
 // Struct to store rectangles and tables
 struct Rectangle {
-    int height = 0;
+    int length = 0;
     int width = 0;
     int area = 0;
 
-    Rectangle(int height, int width) {
-        this->height = height;
+    Rectangle(int length, int width) {
+        this->length = length;
         this->width = width;
-        this->area = height * width;
+        this->area = length * width;
     }
 };
 
@@ -28,19 +28,22 @@ bool compare(Rectangle a, Rectangle b) {
 // Function to find and push to a vector all the maximal rectangles that exists until the given row. 
 // The function uses a stack to store the current rectangle being considered. If a rectangle is in 
 // between smaller rectangles or the ends of the table, push it to the vector.
-void findRectangles(vector<int> &heights, vector<Rectangle> &rectangles, vector<vector<int>> &usedRectangles){
+void findRectangles(vector<int> &lengths, vector<Rectangle> &rectangles, vector<vector<int>> &usedRectangles){
     stack<int> stack;
  
     int top; 
     int i = 0;
-    while (i < heights.size()) {
-        if (stack.empty() || heights[stack.top()] <= heights[i])
-            stack.push(i++);
+    while (i < lengths.size()) {
+        // Add rectangle to the stack if it is not smaller than the rectangle with its index on the top of the stack 
+        if (stack.empty() || !(lengths[stack.top()] > lengths[i])) {
+            stack.push(i);
+            i++;
+        }
         else {
-            top = heights[stack.top()];
+            top = lengths[stack.top()];
             stack.pop();
             if(stack.empty()) {
-                // Only add the rectangle if there is no other rectangle with the same area, including rotated ones
+                // Only add the rectangle if there is no other rectangle with the same area, including rotated ones, and the rectangle's area is greater than 0
                 if (usedRectangles[top][i] == 0 && top * i != 0) {
                     rectangles.push_back(Rectangle(top, i));
                     usedRectangles[top][i] = 1;
@@ -48,6 +51,7 @@ void findRectangles(vector<int> &heights, vector<Rectangle> &rectangles, vector<
                 }
             }
             else {
+                // Only add the rectangle if there is no other rectangle with the same area, including rotated ones, and the rectangle's area is greater than 0
                 if (usedRectangles[top][i - stack.top() - 1] == 0 && top * (i - stack.top() - 1) != 0) {
                     rectangles.push_back(Rectangle(top, i - stack.top() - 1));
                     usedRectangles[top][i - stack.top() - 1] = 1;
@@ -58,9 +62,10 @@ void findRectangles(vector<int> &heights, vector<Rectangle> &rectangles, vector<
     }
     // Find rectangles from the remaining elements in the stack 
     while (!stack.empty()) {
-        top = heights[stack.top()];
+        top = lengths[stack.top()];
         stack.pop();
         if(stack.empty()) {    
+            // Only add the rectangle if there is no other rectangle with the same area, including rotated ones, and the rectangle's area is greater than 0
             if (usedRectangles[top][i] == 0 && top * i != 0) {
                 rectangles.push_back(Rectangle(top, i));
                 usedRectangles[top][i] = 1;
@@ -68,6 +73,7 @@ void findRectangles(vector<int> &heights, vector<Rectangle> &rectangles, vector<
             }
         }
         else {
+            // Only add the rectangle if there is no other rectangle with the same area, including rotated ones, and the rectangle's area is greater than 0
             if (usedRectangles[top][i - stack.top() - 1] == 0 && top * (i - stack.top() - 1) != 0) {
                 rectangles.push_back(Rectangle(top, i - stack.top() - 1));
                 usedRectangles[top][i - stack.top() - 1] = 1;
@@ -84,9 +90,9 @@ int main() {
     cin >> rows >> columns;
 
     // This is a performance optimization to avoid iterating over the biggest dimension below
-    if (columns > rows) {
-        swap(rows, columns);
-    }
+    // if (columns > rows) {
+    //     swap(rows, columns);
+    // }
 
     // Receive the matrix to be considered
     vector<vector<char>> matrix(rows, vector<char>(columns));
@@ -104,9 +110,9 @@ int main() {
     cin >> numberOfTables;
 
     for (int i = 0; i < numberOfTables; i++) {
-        int height, width;
-        cin >> height >> width;
-        tables.push_back(Rectangle(height, width));
+        int length, width;
+        cin >> length >> width;
+        tables.push_back(Rectangle(length, width));
     }
 
     // Sort the tables by area and then by width
@@ -121,17 +127,19 @@ int main() {
 
     // Find all the maximal rectangles from the matrix, calculating the rectangles
     // sizes above a row and then finding the rectangles until this given row
-    vector<int> heightsOfColumns(columns, 0);
+    vector<int> lengthsOfColumns(columns, 0);
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < columns; j++) {
+            // Increase column size if there is an empty space in the [i][j] element
             if (matrix[i][j] == '.') {
-                heightsOfColumns[j]++;
+                lengthsOfColumns[j]++;
             }
+            // Otherwise, reset column size to 0
             else {
-                heightsOfColumns[j] = 0;
+                lengthsOfColumns[j] = 0;
             }
         }
-        findRectangles(heightsOfColumns, rectangles, usedRectangles);
+        findRectangles(lengthsOfColumns, rectangles, usedRectangles);
     }
 
     // Sort the rectangles by area and then by width
@@ -149,7 +157,8 @@ int main() {
         }
         for (int j = 0; j < rectangles.size(); j++) {
             Rectangle rectangle = rectangles[j];
-            if ((table.height <= rectangle.height && table.width <= rectangle.width) || (table.height <= rectangle.width && table.width <= rectangle.height)) {
+            // If the table can be placed in the rectangle, even if it must be rotated, place it and break the loop
+            if ((table.length <= rectangle.length && table.width <= rectangle.width) || (table.length <= rectangle.width && table.width <= rectangle.length)) {
                 maxTable = table;
                 break;
             }
@@ -164,7 +173,7 @@ int main() {
         cout << "-1 1" << endl;
     }
     else {
-        cout << maxTable.height << " " << maxTable.width << endl;
+        cout << maxTable.length << " " << maxTable.width << endl;
     }
 
     return 0;
